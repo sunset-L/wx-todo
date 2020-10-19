7ce5d80dd8eb2363207d5a50b3a4e2ec8e2d6352
liuhuan <liuhuan@kp99.cn>
2020-10-19 17:12:44 +0800

    init

---
+++ miniprogram/pages/todo/index.js
@@ -0,0 +1,24 @@
+// miniprogram/pages/todo/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    todoList: [
+      {content: 1},
+      {content: 1},
+      {content: 1},
+      {content: 1},
+      {content: 1},
+      {content: 1},
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+})
